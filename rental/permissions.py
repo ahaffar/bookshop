549cee8652d52339055fd9dbcaaa97ac77f75269
Ahmed Al-Haffar <alhaffar.ahmed@gmail.com>
2020-06-03 23:36:05 +0300

    Applying model_name and PERMS_MAP for dynamic permission checking

--- rental/permissions.py
+++ rental/permissions.py
@@ -5,6 +5,14 @@ import copy
 # def has_perm(user):
 #     return user.groups.filter(name__in=['librarians', ]).exists()
 
+PERMS_MAP = {
+    'GET': 'view_',
+    'DELETE': 'delete_',
+    'PATCH': 'change_',
+    'POST': 'add_',
+    'PUT': 'change_'
+}
+
 
 class UserUpdatePermission(permissions.BasePermission):
     """
@@ -44,16 +52,17 @@ class UserViewPermissions(permissions.BasePermission):
 
     def has_permission(self, request, view):
         app_label = view.queryset.model._meta.app_label
+        model_name = view.queryset.model._meta.model_name
 
         if request.method == 'GET':
             # return request.user.has_perm('rental.view_user')
-            return request.user.has_perm(str(app_label+'.'+'view_user'))
+            return request.user.has_perm(str(app_label+'.'+PERMS_MAP[request.method]+model_name))
         elif request.method == 'POST':
             return request.user.is_admin()
         elif request.method in ('PUT', 'PATCH'):
-            return request.user.has_perm(str(app_label+'.'+'change_user'))
+            return request.user.has_perm(str(app_label+'.'+PERMS_MAP[request.method]+model_name))
         elif request.method == 'DELETE':
-            return request.user.has_perm(str(app_label+'.'+'delete_user'))
+            return request.user.has_perm(str(app_label+'.'+PERMS_MAP[request.method]+model_name))
         return False
 
     def has_object_permission(self, request, view, obj):
