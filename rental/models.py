3bbcc93196450c92f97d558ecc6f7738531d3703
Ahmed AL Haffar <alhaffra.ahmed@gmail.com>
2020-06-19 20:01:50 +0300

    UserProfile Model Fixin
    - Create a custom UserProfileManager
    - Create a custom queryset to annotate borrow_limit based on user_type
    - Modify due_back field from Borrowed Model

--- rental/models.py
+++ rental/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Case, When, IntegerField, Value
 from bookshop import settings
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, AbstractUser
 from django.contrib.auth import validators
@@ -65,22 +66,6 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.groups.filter(name='librarians').exists()
 
 
-class UserProfile(models.Model):
-    class UserType(models.TextChoices):
-        FREE = 'FR', 'FREE'
-        BASIC = 'BS', 'BASIC'
-        PREMIUM = 'PR', 'PREMIUM'
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    bio = models.CharField(max_length=255)
-    created_on = models.DateTimeField(auto_now_add=True)
-    last_updated = models.DateTimeField(auto_now=True)
-    user_type = models.CharField(max_length=2, choices=UserType.choices, default=UserType.FREE)
-
-    def __str__(self):
-        return self.user.username
-
-
 class Genre(models.Model):
     name = models.CharField(max_length=30, help_text='The name of genre such as Drama, Art...')
 
@@ -118,7 +103,7 @@ class Book(models.Model):
         ES = 'ES', 'SPANISH'
         PT = 'PT', 'PORTUGUESE'
 
-    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, related_name='books')
     author = models.ManyToManyField(Author, related_name='books')
     title = models.CharField(max_length=80, blank=False)
     published_date = models.DateField(null=True, blank=False)
@@ -131,18 +116,69 @@ class Book(models.Model):
 
 
 class Borrowed(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, help_text='username of the borrower', on_delete=models.DO_NOTHING,
-                             related_name='borrowings')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, help_text='username of the borrower',
+                             on_delete=models.DO_NOTHING, related_name='borrowings')
     borrowed_date = models.DateTimeField(auto_now_add=True)
     title = models.ForeignKey(Book, on_delete=models.DO_NOTHING, verbose_name='book')
     returned_date = models.DateField(blank=False, null=True)
     last_updated = models.DateTimeField(auto_now=True, )
     is_returned = models.BooleanField(default=False,)
     # False in order to hide it from admin
-    due_back = models.DateTimeField(default=datetime.now()+timedelta(days=1), editable=False)
+    due_back = models.DateTimeField(default=datetime.now()+timedelta(days=1))
 
     class Meta:
         ordering = ['-borrowed_date']
 
     def __str__(self):
         return '%s - Borrowed By (%s)' % (self.title, self.user)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and datetime.now() > self.due_back:
+            return True
+
+
+# class BookLimit(models.QuerySet):
+#     def book_limit(self):
+#         self.annotate(borrow_limit=Case(When(UserProfile.UserType.FREE, then=Value(1)),
+#                                         When(UserProfile.UserType.BASIC, then=Value(2)),
+#                                         When(UserProfile.UserType.PREMIUM, then=Value(5)),
+#                                         default=Value(1), output_field=IntegerField))
+#
+
+# class ProfilePlanManager(models.Manager):
+#     def get_queryset(self):
+#         return BookLimit(self.model, using=self._db)
+
+
+class UserProfileManager(models.Manager):
+
+    def get_queryset(self):
+        return super().get_queryset().annotate(
+            book_limit=Case(
+                When(user_type=UserProfile.UserType.FREE, then=Value(1)),
+                When(user_type=UserProfile.UserType.BASIC, then=Value(2)),
+                When(user_type=UserProfile.UserType.PREMIUM, then=Value(5)),
+                default=Value(1),
+                output_field=IntegerField()
+            )
+        )
+
+
+class UserProfile(models.Model):
+    class UserType(models.TextChoices):
+        FREE = 'FR', 'FREE'
+        BASIC = 'BS', 'BASIC'
+        PREMIUM = 'PR', 'PREMIUM'
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    user_type = models.CharField(max_length=2, choices=UserType.choices, default=UserType.FREE)
+
+    objects = UserProfileManager()
+
+    def __str__(self):
+        return self.user.username
+
